5a8d3273964eb31f8ad1f6f2f7f23a660eab4263
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-12 10:57:14 -0800

    add ability to check scores

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
   $("#submitAnswers").click(function(event) {
-    var question1 = $("select#question1").val();
-    var question2 = $("select#question2").val();
-    var question3 = $("select#question3").val();
-    var question4 = $("select#question4").val();
-    var question5 = $("select#question5").val();
 
-    /*if () {
+    var javaScore = 0;
+    var cScore = 0;
+    var rubyScore = 0;
+
+    var questions = $("select");
+
+    var k = 1;
+    var answered = "True";
+    for (var i = 0; i < questions.length; i++) {
+      //Validation - Check if they selected an answer other than the default answer of 'Select'
+      var answerCheck = $("select#question"+k).val();
+      if (answerCheck == "Select") {
+        answered = "False";
+      }
+      //Check what answer they gave and add 1 to their score
+      //alert("select#question"+k);
+      var answer = $("select#question"+k).val();
+      if (answer == "c") {cScore++;}
+      if (answer == "ruby") {rubyScore++;}
+      if (answer == "java") {javaScore++;}
+
+      k++;
+    }
+
+    if (answered == "False") {
       alert("Some questions were not answered.");
     } else {
+      alert("cScore"+cScore+" rubyScore"+rubyScore+" javaScore"+javaScore);
 
-    }*/
+    }
+
 
     event.preventDefault();
   });
