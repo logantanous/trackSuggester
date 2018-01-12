c4f8778d65e4b37e7ebc440bb76a7a00222c5756
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-12 14:19:13 -0800

    add results bars

--- js/scripts.js
+++ js/scripts.js
@@ -53,16 +53,31 @@ $(document).ready(function() {
       if (answer == "ruby") {rubyScore++;}
       if (answer == "java") {javaScore++;}
       if (answer == "php") {phpScore++;}
-
-
       k++;
     }
 
     if (answered == "False") {
       alert("Some questions were not answered.");
     } else {
-      alert("cScore"+cScore+" rubyScore"+rubyScore+" javaScore"+javaScore+" phpScore"+phpScore);
-
+      $(".hide").show();      
+      $(".item span").remove();
+      if (javaScore>0) {
+        $(".java").show().css("width", javaScore+"00px").append('<span> '+cScore+"/"+questions.length+'</span>');
+      }
+      if (cScore>0) {
+        $(".c").show().css("width", cScore+"00px").append('<span> '+cScore+"/"+questions.length+'</span>');
+      }
+      if (rubyScore>0) {
+      $(".ruby").show().css("width", rubyScore+"00px").append('<span> '+rubyScore+"/"+questions.length+'</span>');
+      }
+      if (phpScore>0) {
+      $(".php").show().css("width", phpScore+"00px").append('<span> '+phpScore+"/"+questions.length+'</span>');
+      }
+      //remove items that are 0 if they don't refresh
+      if (javaScore == 0){$(".java").hide();}
+      if (phpScore == 0){$(".php").hide();}
+      if (cScore == 0){$(".c").hide();}
+      if (rubyScore == 0){$(".ruby").hide();}
     }
 
 
