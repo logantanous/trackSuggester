4cb140896639ba0e4bcf3c957d5ea98c50caaebc
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-12 09:54:30 -0800

    add more logic and the start of 2 more questions

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#submitAnswers").click(function(event) {
+    var question1 = $("select#question1").val();
+    var question2 = $("select#question2").val();
+    var question3 = $("select#question3").val();
+
+    if (){
+      alert("Some questions were not answered.");
+    } else {
+
+    }
+
+    event.preventDefault();
+  });
+});
