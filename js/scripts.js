48b7f51763e07db6d75c0248b1cea74166953c23
logan tanous1 <logantanous1@Logans-MBP.fios-router.home>
2018-01-12 10:08:34 -0800

    add final questions

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
     var question1 = $("select#question1").val();
     var question2 = $("select#question2").val();
     var question3 = $("select#question3").val();
+    var question4 = $("select#question4").val();
+    var question5 = $("select#question5").val();
 
-    if (){
+    /*if () {
       alert("Some questions were not answered.");
     } else {
 
-    }
+    }*/
 
     event.preventDefault();
   });
